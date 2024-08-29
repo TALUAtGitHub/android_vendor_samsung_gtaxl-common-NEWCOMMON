6fc7b2833824d3c6676b5f1974536c22cd2b1495
K9100ii <k9100ii.ki@gmail.com>
2024-08-29 15:38:42 +0100

    gtaxl-common: Regenerate
    Note libwvhidl has been modified since a dependency replacement for a
    protobuf lib has been redone using Lineage's version of patchelf. I
    manually used my distros version of patchelf before, and they seem to
    work differently.
    
    Change-Id: I3c03fad955afb4e0bda99635ffc8ac6c330919fb

--- gtaxl-common-vendor.mk
+++ gtaxl-common-vendor.mk
@@ -6,32 +6,32 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/samsung/gtaxl-common
 
 PRODUCT_COPY_FILES += \
-    vendor/samsung/gtaxl-common/proprietary/vendor/etc/init/android.hardware.drm@1.4-service.widevine.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.drm@1.4-service.widevine.rc \
     vendor/samsung/gtaxl-common/proprietary/vendor/firmware/Data.msc:$(TARGET_COPY_OUT_VENDOR)/firmware/Data.msc \
     vendor/samsung/gtaxl-common/proprietary/vendor/firmware/bdwlan30.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/bdwlan30.bin \
     vendor/samsung/gtaxl-common/proprietary/vendor/firmware/bdwlan32.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/bdwlan32.bin \
-    vendor/samsung/gtaxl-common/proprietary/vendor/firmware/fimc_is_lib.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/fimc_is_lib.bin \
-    vendor/samsung/gtaxl-common/proprietary/vendor/firmware/mfc_fw.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/mfc_fw.bin \
     vendor/samsung/gtaxl-common/proprietary/vendor/firmware/nvm_tlv_tf_1.1.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvm_tlv_tf_1.1.bin \
     vendor/samsung/gtaxl-common/proprietary/vendor/firmware/otp30.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/otp30.bin \
     vendor/samsung/gtaxl-common/proprietary/vendor/firmware/qwlan30.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/qwlan30.bin \
     vendor/samsung/gtaxl-common/proprietary/vendor/firmware/qwlan30_ibss.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/qwlan30_ibss.bin \
     vendor/samsung/gtaxl-common/proprietary/vendor/firmware/rampatch_tlv_tf_1.1.tlv:$(TARGET_COPY_OUT_VENDOR)/firmware/rampatch_tlv_tf_1.1.tlv \
-    vendor/samsung/gtaxl-common/proprietary/vendor/firmware/setfile_4h5yc.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/setfile_4h5yc.bin \
-    vendor/samsung/gtaxl-common/proprietary/vendor/firmware/setfile_sr259.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/setfile_sr259.bin \
     vendor/samsung/gtaxl-common/proprietary/vendor/firmware/utf30.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/utf30.bin \
     vendor/samsung/gtaxl-common/proprietary/vendor/firmware/utfbd30.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/utfbd30.bin \
-    vendor/samsung/gtaxl-common/proprietary/vendor/firmware/utfbd32.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/utfbd32.bin
+    vendor/samsung/gtaxl-common/proprietary/vendor/firmware/utfbd32.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/utfbd32.bin \
+    vendor/samsung/gtaxl-common/proprietary/vendor/etc/init/android.hardware.drm@1.4-service.widevine.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.drm@1.4-service.widevine.rc \
+    vendor/samsung/gtaxl-common/proprietary/vendor/firmware/fimc_is_lib.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/fimc_is_lib.bin \
+    vendor/samsung/gtaxl-common/proprietary/vendor/firmware/mfc_fw.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/mfc_fw.bin \
+    vendor/samsung/gtaxl-common/proprietary/vendor/firmware/setfile_4h5yc.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/setfile_4h5yc.bin \
+    vendor/samsung/gtaxl-common/proprietary/vendor/firmware/setfile_sr259.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/setfile_sr259.bin
 
 PRODUCT_PACKAGES += \
-    libGLES_mali \
     camera.vendor.universal7870 \
-    sensors.universal7870 \
     libexynoscamera \
     libexynoscamera3 \
     libsensorlistener \
-    libstainkiller \
     libuniplugin \
+    libGLES_mali \
+    sensors.universal7870 \
+    libstainkiller \
     libwvhidl \
     android.hardware.drm@1.4-service.widevine \
     wcnss_filter
